4870e6a199b4988e8dc215545ca770f953e69f2c
Easit AB <github@easit.com>
2023-04-04 09:44:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Easit.GO.Webservice
+
+This is the repository for the PowerShell 7 module for Easit GO webAPI.
+
+## Project status
+
+[![Test source with PSSA and Pester](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/source-run-pssa-and-pester.yml/badge.svg)](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/source-run-pssa-and-pester.yml)
+
+[![Test tests with Pester](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/tests-run-pester.yml/badge.svg)](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/tests-run-pester.yml)
+
+[![Release notes](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/release-drafter.yml/badge.svg)](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/release-drafter.yml)
+
+[![Publish module](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/publishmodule.yml/badge.svg?branch=main)](https://github.com/easitab/Easit.GO.Webservice/actions/workflows/publishmodule.yml)
+
+## Docs & Introduction
+
+All documentation regarding Easit.GO.Webservice can be found [here](https://docs.easitgo.com/techspace/psmodules/intro/) and an "intro" to Easit GO WebAPI can be found [here](https://docs.easitgo.com/techspace/easitgo/webapi/v1/intro/)
+
+## Support & Questions
+
+Questions and issue can be sent to [supportATeasit](mailto:support@easit.com) or open a issue from the Issues tab and label it with one of the predefined labels.
+
+You can also join our Slack chanel [gowebservice here](https://join.slack.com/t/easit-powershell/shared_invite/zt-smxca3na-0CqUMNpOP2W8DoCiMU0MgQ).
---
+++ build/publishmodule.ps1
@@ -0,0 +1,105 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string]$CompanyName,
+    [Parameter(Mandatory)]
+    [string]$ModuleName,
+    [Parameter(Mandatory)]
+    [string]$Tag,
+    [Parameter(Mandatory)]
+    [string]$PSGalleryKey,
+    [Parameter(Mandatory)]
+    [string]$GitHubBaseURI,
+    [Parameter(Mandatory)]
+    [string]$TechspaceBaseURI,
+    [Parameter(Mandatory)]
+    [string]$ModuleDescription,
+    [Parameter(Mandatory)]
+    [string]$ModulePSVersion,
+    [Parameter(Mandatory)]
+    [string]$ModuleAuthor,
+    [Parameter(Mandatory)]
+    [string]$Copyright
+)
+
+begin {
+    Write-Host "Publish module script start"
+}
+
+process {
+    $repoRoot = Split-Path -Path $PSScriptRoot -Parent
+    $sourceRoot = Join-Path $repoRoot -ChildPath 'source'
+    $tempBuildDirectory = Join-Path $repoRoot -ChildPath 'temp'
+    if (Test-Path -Path $tempBuildDirectory) {
+        try {
+            Get-ChildItem -Path $tempBuildDirectory -Recurse -ErrorAction Stop | Remove-Item -Confirm:$false -Recurse -ErrorAction Stop
+        } catch {
+            Write-Warning "Unable to clean $tempBuildDirectory"
+            throw $_
+        }
+    }
+    if (!(Test-Path -Path $sourceRoot)) {
+        throw "Cannot find $sourceRoot"
+    }
+    try {
+        $allScripts = Get-ChildItem -Path "$sourceRoot" -Filter "*.ps1" -Recurse -ErrorAction Stop
+    } catch {
+        throw $_
+    }
+    if (!$allScripts) {
+        throw "No scripts or functions found"
+    }
+    $moduleRoot = New-Item -Path $tempBuildDirectory -Name $ModuleName -ItemType Directory
+    $psm1 = New-Item -Path $moduleRoot -Name "$ModuleName.psm1" -ItemType File
+    Write-Host "Generating new psm1"
+    foreach ($script in $allScripts) {
+        $scriptContent = $null
+        try {
+            $scriptContent = Get-Content -Path $script.FullName -Raw
+        } catch {
+            Write-Warning "Failed to get content from $($script.FullName)"
+            throw $_
+        }
+        try {
+            Add-Content -Path $psm1 -Value $scriptContent
+        } catch {
+            Write-Warning "Failed to add content to $($psm1.FullName)"
+            throw $_
+        }
+    }
+    $manifestFilePath = Join-Path -Path "$moduleRoot" -ChildPath "$ModuleName.psd1"
+    $manifest = @{
+        Path              = "$manifestFilePath"
+        RootModule        = "$moduleName.psm1"
+        CompanyName       = "$CompanyName"
+        Author            = "$ModuleAuthor"
+        ModuleVersion     = "$Tag"
+        HelpInfoUri       = "$TechspaceBaseURI/psmodules/intro/"
+        LicenseUri        = "$GitHubBaseURI/$ModuleName/blob/main/LICENSE"
+        ProjectUri        = "$GitHubBaseURI/$ModuleName"
+        Description       = "$ModuleDescription"
+        PowerShellVersion = "$ModulePSVersion"
+        Copyright         = "$Copyright"
+    }
+    try {
+        Write-Host "Creating new module manifest"
+        New-ModuleManifest @manifest -ErrorAction Stop | Out-Null
+    } catch {
+        Write-Warning "Failed to create new module manifest"
+        throw $_
+    }
+    if (Test-ModuleManifest -Path "$manifestFilePath") {
+        Write-Host "Publishing module to PSGallery"
+        try {
+            Publish-Module -Path "$moduleRoot" -NuGetApiKey "$PSGalleryKey"
+        } catch {
+            Write-Warning "Failed to publish module to gallery"
+            throw $_
+        }
+        Write-Host "Module published!"
+    }
+}
+
+end {
+    Write-Host "Publish module script end"
+}
