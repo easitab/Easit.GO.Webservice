22d705646ed279957fe34a8a217e99f4ace07c91
Easit AB <github@easit.com>
2023-04-05 16:15:49 +0200

    remove XML example in New-PingRequestObject

--- source/private/New-PingRequestObject.ps1
+++ source/private/New-PingRequestObject.ps1
@@ -4,15 +4,6 @@ function New-PingRequestObject {
         Creates a XML object that can be used for sending a ping request to Easit GO.
     .DESCRIPTION
         Creates a new baseline XML request object with *New-XMLRequestObject* and add the *PingRequest* element to that object.
-
-        ```xml
-        <soapenv:Envelope xmlns:sch="http://www.easit.com/bps/schemas" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-            <soapenv:Header />
-            <soapenv:Body>
-                <sch:PingRequest>?</sch:PingRequest>
-            </soapenv:Body>
-        </soapenv:Envelope>
-        ```
     .EXAMPLE 
         New-XMLRequestObject -NamespaceURI "http://schemas.xmlsoap.org/soap/envelope/" -NamespaceSchema "http://www.easit.com/bps/schemas"
     .PARAMETER NamespaceURI
