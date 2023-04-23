6365ec8304a76a683916bd718ece6e89ceaa186d
Easit AB <github@easit.com>
2023-04-23 12:28:43 +0200

    update comment based help

--- source/private/New-ItemToImportAttachmentXMLObject.ps1
+++ source/private/New-ItemToImportAttachmentXMLObject.ps1
@@ -1,26 +1,32 @@
 function New-ItemToImportAttachmentXMLObject {
     <#
     .SYNOPSIS
-        Creates a attachment XML element that can be used for sending requests to Easit GO.
+        Creates a attachment XML element for attachments.
     .DESCRIPTION
-        Creates a attachment XML element, assigns it to the script variable *schItemToImport*.
-        The element will have a attribute called *name* with a value of the input for the Name parameter.
-        If the Value parameter does not contain *[IO.Path]::DirectorySeparatorChar*, the value be treated as a base64 string.
+        Creates a attachment XML element and appends it to requestItemToImportElement.
+        The element will have a attribute called *name* with the value of the input for the Name parameter.
+        If the Value parameter does not contain *[IO.Path]::DirectorySeparatorChar*, the value will be treated as a base64 string.
         
         If the Value parameter contains *[IO.Path]::DirectorySeparatorChar*, the value be treated as a path. If the path failes 
         *Test-Path* an error is thrown, otherwise we convert the content of the file to base64 and set that string as innerText.
-        If the value is a path and the Name parameter is empty, the filename will be used as value for the name attribute.
+        If in put for Value is a path and the Name parameter is empty, the filename will be used as value for the name attribute.
         
     .EXAMPLE
-        foreach ($property in $InputObject.psobject.properties) {
-            New-ItemToImportPropertyXMLObject -NamespaceSchema $NamespaceSchema -Name $property.Name -Value $property.Value
+        if ($property.Value -is [hashtable]) {
+            if ($property.Name -eq 'Attachment' -OR $property.Name -eq 'Attachments') {
+                foreach ($attachment in $property.Value.GetEnumerator()) {
+                    New-ItemToImportAttachmentXMLObject -NamespaceSchema $NamespaceSchema -Name $attachment.Name -Value $attachment.Value -RequestPrefix $RequestPrefix
+                }
+            }
         }
     .PARAMETER NamespaceSchema
-        URI to schema used to for the body elements namespace (xmlns:sch).
+        URI used for the body elements namespace.
     .PARAMETER Name
         Value to be set as value for name attribute.
     .PARAMETER Value
         Value to be set as innertext for element.
+    .PARAMETER RequestPrefix
+        Prefix used for elements appended to the Body element.
     #>
     [CmdletBinding()]
     param (
@@ -41,11 +47,15 @@ function New-ItemToImportAttachmentXMLObject {
     process {
         $directorySeparator = [IO.Path]::DirectorySeparatorChar
         $base64string = $null
+        if ([string]::IsNullOrWhiteSpace($Name) -and [string]::IsNullOrWhiteSpace($Value)) {
+            throw "Name and value is null or empty"
+        }
         if ($Value -match "\${directorySeparator}") {
             if (Test-Path -Path (Resolve-Path -Path $Value)) {
                 try {
                     $base64string = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes((Resolve-Path -Path $Value)))
                 } catch {
+                    Write-Warning "Failed to convert file content to base 64"
                     throw $_
                 }
                 if ([string]::IsNullOrWhiteSpace($Name) -or $Name -match 'null') {
@@ -54,11 +64,12 @@ function New-ItemToImportAttachmentXMLObject {
             } else {
                 throw "Cannot find $Value"
             }
-        } else {
-            $base64string = $Value
+        } 
+        if (!($Value -match "\${directorySeparator}")) {
             if ([string]::IsNullOrWhiteSpace($Name)) {
                 throw "Missing name for attachment"
             }
+            $base64string = $Value
         }
         try {
             $requestItemToImportAttachmentElement = $requestXMLObject.CreateElement("${RequestPrefix}:Attachment","$NamespaceSchema")
