1d6570db4669cc967b267074ce3d8801084b24b1
Easit AB <github@easit.com>
2023-07-20 08:29:05 +0200

    add documentation to 'Convert-AttachmentElement'

--- source/private/Convert-AttachmentElement.ps1
+++ source/private/Convert-AttachmentElement.ps1
@@ -1,4 +1,22 @@
 function Convert-AttachmentElement {
+    <#
+    .SYNOPSIS
+        Converts an attachment element to a PSCustomObject.
+    .DESCRIPTION
+        The **Convert-AttachmentElement** takes an XML element and creates a PSCustomObject from that element.
+        The name attribute and innerText is used directly from the XML element. 
+
+        **Convert-AttachmentElement** also adds a property named *ContentLength* to the return object with the value of
+        *[element].InnerText.Length*.
+    .EXAMPLE
+        if ($ChildElement.LocalName -eq 'Attachment') {
+            Convert-AttachmentElement -Attachment $ChildElement
+        }
+    .PARAMETER Attachment
+        The XML element to be converted to a PSCustomObject
+    .OUTPUTS
+        [PSCustomObject](https://learn.microsoft.com/en-us/dotnet/api/system.management.automation.pscustomobject)
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
