f4806e7516c260f7bd04ca8ba9bfaf358694585c
Easit AB <github@easit.com>
2023-07-25 18:54:21 +0200

    add documentation to 'New-ReturnObject'

--- source/private/New-ReturnObject.ps1
+++ source/private/New-ReturnObject.ps1
@@ -1,4 +1,23 @@
 function New-ReturnObject {
+    <#
+    .SYNOPSIS
+        Creates an base PSCustomObject returned by *Convert-GetItemsResponse*.
+    .DESCRIPTION
+        **New-ReturnObject** creates a "base" object with some properties that a GetItemsResponse always contains.
+    .EXAMPLE
+        $Response.Envelope.Body.GetItemsResponse.Items.GetEnumerator() | ForEach-Object -Parallel {
+            $xmlResponse = $using:Response
+            try {
+                $returnObject = New-ReturnObject -XML $xmlResponse
+            } catch {
+                throw $_
+            }
+        }
+    .PARAMETER XML
+        XML object to be used as basis for a new base object.
+    .OUTPUTS
+        [PSCustomObject](https://learn.microsoft.com/en-us/dotnet/api/system.management.automation.pscustomobject)
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -8,7 +27,11 @@ function New-ReturnObject {
         Write-Verbose "$($MyInvocation.MyCommand) initialized"
     }
     process {
-        $returnObject = New-Object PSCustomObject
+        try {
+            $returnObject = New-Object PSCustomObject
+        } catch {
+            throw $_
+        }
         try {
             $returnObject | Add-Member -MemberType NoteProperty -Name "RequestedPage" -Value "$($XML.Envelope.Body.GetItemsResponse.requestedPage)"
             $returnObject | Add-Member -MemberType NoteProperty -Name "TotalNumberOfPages" -Value "$($XML.Envelope.Body.GetItemsResponse.totalNumberOfPages)"
