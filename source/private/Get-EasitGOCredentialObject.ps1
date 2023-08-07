a6756019901f9274a2c5a5328fa41a41b9cebf35
Easit AB <github@easit.com>
2023-08-07 09:17:15 +0200

    add function 'Get-EasitGOCredentialObject'

---
+++ source/private/Get-EasitGOCredentialObject.ps1
@@ -0,0 +1,42 @@
+function Get-EasitGOCredentialObject {
+    <#
+    .SYNOPSIS
+        Creates a PSCredential object.
+    .DESCRIPTION
+        **Get-EasitGOCredentialObject** creates a credential object that can be used when sending requests to Easit GO WebAPI.
+    .EXAMPLE
+        Get-EasitGOCredentialObject -Apikey $Apikey
+
+        UserName                     Password
+        --------                     --------
+                 System.Security.SecureString
+    .PARAMETER Apikey
+        APIkey used for authenticating to Easit GO WebAPI.
+    .OUTPUTS
+        [PSCredential](https://learn.microsoft.com/en-us/dotnet/api/system.management.automation.pscredential)
+    #>
+    [OutputType('PSCredential')]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Apikey
+    )
+    begin {
+        Write-Verbose "$($MyInvocation.MyCommand) initialized"
+    }
+    process {
+        try {
+            [securestring]$secString = ConvertTo-SecureString $Apikey -AsPlainText -Force
+        } catch {
+            throw $_
+        }
+        try {
+            New-Object System.Management.Automation.PSCredential (' ', $secString)
+        } catch {
+            throw $_
+        }
+    }
+    end {
+        Write-Verbose "$($MyInvocation.MyCommand) completed"
+    }
+}
