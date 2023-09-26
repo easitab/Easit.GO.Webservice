a14cd20162c141c0ec6a15ed011ed80e2d07ebfa
Easit AB <github@easit.com>
2023-09-26 15:08:51 +0200

    update test template

--- tests/data/testTemplate.ps1
+++ tests/data/testTemplate.ps1
@@ -25,7 +25,10 @@ Describe "XXXX" -Tag 'function','private' {
     It 'help section should have EXAMPLES' {
         ((Get-Help "$($envSettings.CommandName)" -Full).EXAMPLES).Length | Should -BeGreaterThan 0
     }
-    It '"Get-BaseRestMethodParameters" should return xxxx' {
-        (Get-BaseRestMethodParameters -Ping) | Should -BeOfType XXX
+    It 'should return xxxx' {
+        #My-Command | Should -BeOfType XXX
+    }
+    It 'should throw' {
+        #{My-Command} | Should -Throw
     }
 }
