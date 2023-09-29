d4ef77983082502fcdab286ac98ca20e635d3ff6
Easit AB <github@easit.com>
2023-09-29 09:53:46 +0200

    remove help section from class

--- source/classes/SortColumn.Class.ps1
+++ source/classes/SortColumn.Class.ps1
@@ -1,18 +1,3 @@
-<#
-.SYNOPSIS
-    Initializes a new instance of the SortColumn class.
-.DESCRIPTION
-    Initializes a new instance of the SortColumn class with the provided input. This class only have 1 contructor and that contructor looks like this:
-
-    - [SortColumn]::New(String,String)
-
-    Available methods:
-    - ToPSCustomObject (Used by private functions)
-.EXAMPLE
-    $SortColumn = [SortColumn]::New($Name,$Order)
-.EXAMPLE
-    $SortColumn.ConvertToPSCustomObject()
-#>
 Class SortColumn {
     [String]$Name
     [ValidateSet('Ascending','Descending')]
