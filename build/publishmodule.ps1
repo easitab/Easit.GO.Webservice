fd52482b2e41c4161e470afd065fd3b8c9c315ad
Easit AB <github@easit.com>
2023-10-04 09:31:07 +0200

    remove steps for generating docs

--- build/publishmodule.ps1
+++ build/publishmodule.ps1
@@ -28,27 +28,10 @@ begin {
     Write-Information "Script start"
 }
 process {
-    try {
-        Install-Module -Name 'platyPS' -Scope CurrentUser -Force -ErrorAction Stop
-        Import-Module 'platyPS' -Force -ErrorAction Stop
-    } catch {
-        throw $_
-    }
     $repoDirectory = Split-Path -Path $PSScriptRoot -Parent
     $sourceDirectory = Join-Path $repoDirectory -ChildPath 'source'
     if (!(Test-Path -Path $sourceDirectory)) {
         throw "Cannot find $sourceDirectory"
-    }
-    $docsDirectory = Join-Path -Path $repoDirectory -ChildPath 'docs'
-    if (Test-Path -Path $docsDirectory) {
-        Write-Information "$docsDirectory already exist"
-    } else {
-        try {
-            Write-Information "Creating $docsDirectory"
-            $null = New-Item -Path $repoDirectory -Name 'docs' -ItemType Directory
-        } catch {
-            throw $_
-        }
     }
     $tempBuildDirectory = Join-Path $repoDirectory -ChildPath 'temp'
     if (Test-Path -Path $tempBuildDirectory) {
@@ -64,18 +47,6 @@ process {
             Write-Information "Creating $tempBuildDirectory"
             $null = New-Item -Path $repoDirectory -Name 'temp' -ItemType Directory
         } catch {
-            throw $_
-        }
-    }
-    $tagDocsDirectory = Join-Path -Path $docsDirectory -ChildPath $Tag
-    if (Test-Path -Path $tagDocsDirectory) {
-        try {
-            Write-Information "Cleaning files in $tagDocsDirectory"
-            Get-ChildItem -Path $tagDocsDirectory -Recurse -File | Remove-Item -Confirm:$false
-            Write-Information "Cleaning folders in $tagDocsDirectory"
-            Get-ChildItem -Path $tagDocsDirectory -Recurse -Directory | Remove-Item -Confirm:$false
-        } catch {
-            Write-Warning "Unable to clean $tagDocsDirectory"
             throw $_
         }
     }
@@ -179,57 +150,17 @@ process {
         }
         Write-Information "Module published!"
     }
-    try {
-        Write-Information "Importing module ($psm1) to session"
-        Import-Module $psm1 -Force
-    } catch {
-        throw $_
-    }
-    try {
-        Write-Information "Generating markdown help for module $ModuleName to $tagDocsDirectory"
-        $null = New-MarkdownHelp -Module $ModuleName -OutputFolder $tagDocsDirectory -Force
-    } catch {
-        throw $_
-    }
-    $privateDocsDirectory = Join-Path -Path $tagDocsDirectory -ChildPath 'private'
-    if (Test-Path -Path $privateDocsDirectory) {
-        Get-ChildItem -Path $privateDocsDirectory -Recurse -File | Remove-Item -Confirm:$false
-    } else {
-        $null = New-Item -Path $tagDocsDirectory -Name 'private' -ItemType Directory
-    }
-    Write-Information "Moving markdown files from $tagDocsDirectory to $privateDocsDirectory"
-    foreach ($privateFunction in $privateFunctions) {
-        $privateMDFile = Get-ChildItem -Path $tagDocsDirectory -Recurse -Include "$($privateFunction.BaseName).md"
-        if ($privateMDFile) {
-            try {
-                Move-Item -Path $privateMDFile.FullName -Destination $privateDocsDirectory
-            } catch {
-                throw $_
-            }
-        } else {
-            Write-Warning "Unable to find $($privateFunction.BaseName).md"
+    $moduleVersionRoot = Join-Path -Path $moduleRoot -ChildPath $Tag
+    $publishedModules = Join-Path -Path $repoDirectory -ChildPath 'publishedModules'
+    if (Test-Path -Path $moduleVersionRoot) {
+        try {
+            Move-Item -Path $moduleVersionRoot -Destination "$publishedModules\"
+        } catch {
+            Write-Warning "Failed to move $moduleVersionRoot to $publishedModules"
         }
-    }
-    $publicDocsDirectory = Join-Path -Path $tagDocsDirectory -ChildPath 'public'
-    if (Test-Path -Path $publicDocsDirectory) {
-        Get-ChildItem -Path $publicDocsDirectory -Recurse -File | Remove-Item -Confirm:$false
     } else {
-        $null = New-Item -Path $tagDocsDirectory -Name 'public' -ItemType Directory
+        Write-Warning "$moduleVersionRoot does not exist, please move published module manually to $publishedModules"
     }
-    Write-Information "Moving markdown files from $tagDocsDirectory to $publicDocsDirectory"
-    foreach ($publicFunction in $publicFunctions) {
-        $publicMDFile = Get-ChildItem -Path $tagDocsDirectory -Recurse -Include "$($publicFunction.BaseName).md"
-        if ($publicMDFile) {
-            try {
-                Move-Item -Path $publicMDFile.FullName -Destination $publicDocsDirectory
-            } catch {
-                throw $_
-            }
-        } else {
-            Write-Warning "Unable to find $($publicFunction.BaseName).md"
-        }
-    }
-    Write-Information "Module documentation for release complete"
 }
 end {
     Write-Information "Script end"
